b35f10aa549f648d61e91b4acaae31fb4770967c
Chetan Pathak <70676486+Chetandev16@users.noreply.github.com>
2022-02-12 09:17:49 +0530

    Delete .history directory

--- .history/src/App_20220210185358.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const App = () => {
-  return (
-    <div>CryptoHub</div>
-  )
-}
-
-export default App
--- .history/src/components/index_20220210193333.js
+++
@@ -1,4 +0,0 @@
-export { default as Navbar } from "./Navbar";
-export { default as Exchanges } from "./Exchanges";
-export { default as Navbar } from "./Navbar";
-export { default as Navbar } from "./Navbar";
